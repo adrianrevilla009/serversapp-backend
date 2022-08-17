3e68f952ee419c78d2844657063f040541730c55
adrianrevilla009 <adrianrg1996@gmail.com>
2022-08-17 20:45:50 +0200

    Spring initializr

---
+++ src/main/java/com/adrian/serversapp/ServersappApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.serversapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrian/serversapp/ServersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.serversapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
